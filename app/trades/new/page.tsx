88a6ce7fb021a7283fa622f5325db2d4e91b8617
roquerodriguez2014 <roquear10@gmail.com>
2026-05-14 18:16:31 -0300

    Add SL and TP fields

--- app/trades/new/page.tsx
+++ app/trades/new/page.tsx
@@ -194,7 +194,8 @@ export default function NewTrade() {
             <p className="text-[13px] font-semibold mb-4">
               Datos de la operación
             </p>
-           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3">
+           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3 mt-3"></div>
               {/* ── ACTIVO CON AUTOCOMPLETE ── */}
               <div className="relative">
                 <label className={labelClass}>Activo</label>
